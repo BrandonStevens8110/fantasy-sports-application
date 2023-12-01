4368771f185f015b545433274e54daf1415d5261
BrandonStevens8110 <Boothomas.2000@gmail.com>
2023-12-01 10:42:54 -0800

    first commit

---
+++ src/main/java/com/fantasy/sports/fantasysports/FantasySportsApplication.java
@@ -0,0 +1,13 @@
+package com.fantasy.sports.fantasysports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasySportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasySportsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fantasy/sports/fantasysports/FantasySportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fantasy.sports.fantasysports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasySportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
